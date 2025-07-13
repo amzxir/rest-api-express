414dd0981be0f16f5e3f96b1f4f525ce946233e1
amzxir <amirreza79ahmadi@gmail.com>
2025-07-13 10:53:08 +0330

    chnage model place user

--- models/place.js
+++ models/place.js
@@ -7,11 +7,11 @@ const placeSchema = new Schema({
   description: { type: String, required: true },
   address: { type: String, required: true },
   image: { type: String, required: true },
-  creator: { type: String, required: true },
+  creator: { type: mongoose.Types.ObjectId, required: true , ref:"User" },
   location: {
     lat: { type: String, required: true },
     lng: { type: String, required: true },
   },
 });
 
-module.exports = mongoose.model("place", placeSchema);
+module.exports = mongoose.model("Place", placeSchema);
