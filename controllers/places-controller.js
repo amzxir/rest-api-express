20863fdbaaba06e7a0bd5ebe4587a77ee44d611b
amzxir <amirreza79ahmadi@gmail.com>
2025-07-25 18:06:58 +0330

    fixed bug user id

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -74,7 +74,7 @@ const createPlace = async (req, res, next) => {
   const createPlace = new Place({
     title,
     address,
-    creator,
+    creator: req.userData.userId,
     description,
     image: imagePath,
     location: coordinates,
@@ -82,7 +82,7 @@ const createPlace = async (req, res, next) => {
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch {
     const err = new HttpError("Created plase failed , please try again.", 500);
     return next(err);
